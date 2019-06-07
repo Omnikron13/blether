1033c65d9df2b7a274910ff029d53032a7cee257
Joey Sabey <GameFreak7744@gmail.com>
2019-06-07 08:15:16 +0100

    Adding rss cache and etag field to feed db table

--- sql/feeds.sql
+++ sql/feeds.sql
@@ -3,5 +3,7 @@ CREATE TABLE IF NOT EXISTS feeds(
     url      TEXT NOT NULL UNIQUE COLLATE NOCASE,
     title    TEXT,
     subtitle TEXT,
+    cached   TEXT,
+    etag     TEXT,
     updated  INTEGER
 );
