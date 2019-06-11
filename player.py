a39fe8ee3f2b36a1b2cbe361f32dd4325e19ac5f
Joey Sabey <GameFreak7744@gmail.com>
2019-06-11 03:03:07 +0100

    Added (very preliminary) play() method to Player

--- player.py
+++ player.py
@@ -7,3 +7,8 @@ class Player(metaclass=Singleton):
     def __init__(self):
         self.instance = vlc.Instance()
         self.player = self.instance.media_player_new()
+
+    def play(self, file):
+        media = self.instance.media_new(file)
+        self.player.set_media(media)
+        self.player.play()
